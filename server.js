e48b016e4079e390e1a45aab4ea76485e704fa39
Tierra Rogers <tierragore@yahoo.com>
2021-05-18 21:29:41 -0400

    Updating the server.js file

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
